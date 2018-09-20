139307818e64f17c3717774747d93660507dd321
Thomsath <thomas.beaupertuis@hotmail.fr>
2018-09-20 18:19:55 +0200

    Add id to user

--- src/AppBundle/Model/User.php
+++ src/AppBundle/Model/User.php
@@ -5,13 +5,19 @@ namespace AppBundle\Model;
 class User { 
 
 	private $name;
+	private $id;
 
-	function __construct($Nname) {
+	function __construct($Nname, $nID) {
 		$this->name = $Nname;
+		$this->id = $nID;
 	}
 
 	public function getName() {
 		return $this->name;
+	}
+
+	public function getID() {
+		return $this->id;
 	}
 
 	public function setName($Nname) {
