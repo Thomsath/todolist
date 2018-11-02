8b9a52b8be0a94ad7b3f299243cded306355226b
Thomsath <thomas.beaupertuis@hotmail.fr>
2018-11-02 14:08:26 +0100

    Edit User entity + add task form

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -10,10 +10,12 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use AppBundle\Entity\Task;
 use AppBundle\Entity\User;
 
 use AppBundle\Loader\TaskLoader;
+use AppBundle\Loader\UserLoader;
 
 class TaskController extends Controller
 {
@@ -21,9 +23,11 @@ class TaskController extends Controller
      * @Route("/", name="homepage")
      */
 
-    public function indexAction(Request $request, TaskLoader $taskloader) {
+    public function indexAction(Request $request, TaskLoader $taskloader, UserLoader $userloader) {
 
     	$tasks = $taskloader->findAllTasks();
+
+        $users = $userloader->findAllUsers();
 
     	return $this->render('task/index.html.twig', [
             'tasks' => $tasks
@@ -34,39 +38,42 @@ class TaskController extends Controller
      * @Route ("/newTask", name="newTask")
      */
 
-    public function addTask(Request $request) {
+    public function addTask(Request $request, UserLoader $userloader) {
 
     	$task = new Task();
 
-    	$form = $this->createFormBuilder($task)
-    			->add('title', TextType::class)
-    			->add('content', TextType::class)
-    			->add('priority', CheckboxType::class)
-    			->add('done', CheckboxType::class)
-    			->add('save', SubmitType::class, array('label' => 'Ajouter la tache'))
+        $users = $userloader->findAllUsers();
+        $selectUsers = [];
+        
+        // ['thomas' => 'thomas']
+        foreach($users as $user) {
+            $selectUsers[$user->getName()] = $user;
+        }
+dump($selectUsers);
+        $form = $this->createFormBuilder($task)
+    			->add('title', TextType::class, array('required' => true,))
+    			->add('content', TextType::class, array('required' => true,))
+    			->add('priority', CheckboxType::class, array('required' => false,))
+    			->add('done', CheckboxType::class, array('required' => false,))
+                ->add('user', ChoiceType::class, array(
+                    'choices' => $selectUsers,
+                ))
+                ->add('save', SubmitType::class, array('label' => 'Ajouter la tache'))
     			->getForm();
  		
  		$form->handleRequest($request);
 		
 		if($form->isSubmitted() && $form->isValid()) {
-			$customUser = new User();
+            $form->getData();
 
-			$customUser->setName('Thomas');
-		    // $form->getData() holds the submitted values
-		    // but, the original `$task` variable has also been updated
-		    $task = $form->getData();
-			$task->setUser($customUser);
-		    // ... perform some action, such as saving the task to the database
-		    // for example, if Task is a Doctrine entity, save it!
-		    
-		    $TaskManager = $this->getDoctrine()->getManager();
+            $TaskManager = $this->getDoctrine()->getManager();
 		    $TaskManager->persist($task);
-		    $TaskManager->flush();
+            $TaskManager->flush();
 
 		    return $this->redirectToRoute('homepage');
 		}
 			return $this->render('task/newTask.html.twig', array(
-					'form' => $form->createView(),
-	));
+				'form' => $form->createView(),
+	       ));
     }
 }
